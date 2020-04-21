bc9ab69bdd12285fa4f58c8e39463b73bc7847fa
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-04-21 17:40:53 +0200

    nixpkgs: landing-zone: add pkgs
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ nixpkgs/landing-zone-debug/default.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "landing-zone";
+  version = "0.3.0+git";
+
+  src = ./../../..;
+
+  patches = [ ./compatibility.patch ];
+
+  nativeBuildInputs = [ hexdump pkgconfig automake gcc-unwrapped ];
+  buildInputs = [ libstdcxx5 ];
+
+  meta = with lib; {
+    homepage = https://github.com/TrenchBoot/landing-zone;
+    description = "Landing Zone";
+    license = licenses.gpl2;
+    platforms = platforms.all;
+  };
+
+  makeFlags = [ "DEBUG=y" ];
+
+}
---
+++ nixpkgs/landing-zone/default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "landing-zone";
+  version = "0.3.0-10-gb686582";
+
+  src = ./../../..;
+
+  patches = [ ./compatibility.patch ];
+
+  nativeBuildInputs = [ hexdump pkgconfig automake gcc-unwrapped ];
+  buildInputs = [ libstdcxx5 ];
+
+  meta = with lib; {
+    homepage = https://github.com/TrenchBoot/landing-zone;
+    description = "Landing Zone";
+    license = licenses.gpl2;
+    platforms = platforms.all;
+  };
+}
