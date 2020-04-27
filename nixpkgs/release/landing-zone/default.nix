52fee324d6fe7b920fd7dd6e76c007e0afa8029f
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-04-27 21:36:13 +0200

    add gitlab-ci support
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ nixpkgs/release/landing-zone/default.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "landing-zone";
+  version = "0.3.0";
+
+  src = builtins.fetchGit {
+  url = "https://github.com/TrenchBoot/landing-zone.git";
+  ref = "master";
+  rev = "89fc4113166823268b07d27f8b13d82223a2361d";
+  };
+
+  patches = [ ./compatibility.patch ];
+
+  nativeBuildInputs = [ hexdump pkgconfig automake gcc-unwrapped ];
+  buildInputs = [ libstdcxx5 ];
+
+  meta = with lib; {
+    homepage = https://github.com/TrenchBoot/landing-zone;
+    description = "Landing Zone";
+    license = licenses.gpl2;
+    platforms = platforms.all;
+  };
+}
